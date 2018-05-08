2f143b82a4bd0c7918effb35a4a067611405fcaa
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-05-08 16:23:38 +0700

    Basic setup

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+console.log('server started');
+
+app.get('/', function (req, res) {
+    res.render('index.ejs');
+});
+
+app.listen(3000);
